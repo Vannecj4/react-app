b142a4f1c9bd2315d882246a904bbc3d19337b73
Vanne <kvcj4@gmail.com>
2020-12-23 16:58:23 +0100

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App holaaaaaa 
+     <h1> Weather App </h1>
+     <footer>
+       This project was coded by Vanessa Cerdas and is {""}
+       <a href= "https://github.com/Vannecj4/react-app" target= "_blank" 
+       > 
+       Open source on Github 
+       </a>
+       </footer> 
     </div>
+    
   );
 }
 
