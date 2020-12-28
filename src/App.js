73adc81e13ed65760265f71a6c6449b86ee4c0a9
Vanne <kvcj4@gmail.com>
2020-12-28 12:07:22 +0100

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div class="container">
      <h1> Weather App </h1>
+     <button class="btn btn-primary"> Hello </button>
      <footer>
        This project was coded by Vanessa Cerdas and is {""}
        <a href= "https://github.com/Vannecj4/react-app" target= "_blank" 
@@ -11,9 +13,10 @@ export default function App() {
        Open source on Github 
        </a>
        </footer> 
+        </div>
     </div>
     
   );
 }
 
- 
+  
