8efa0681facb3e9d908f9195464167946b161bf3
Vanne <kvcj4@gmail.com>
2020-12-29 14:00:08 +0100

    fahrenheit added

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,8 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import WeatherIcon from "./WeatherIcon"
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
 
 export default function WeatherInfo(props) {
     return (
@@ -14,6 +16,7 @@ export default function WeatherInfo(props) {
         </ul>
         <div className="row mt-3">
         <div className="col-6">
+
             <div className="clearfix">
                 <div className="float-left">
                 <WeatherIcon 
@@ -22,8 +25,9 @@ export default function WeatherInfo(props) {
                 </div>
 
 <div className="float-left">
-       <span className="temperature">{Math.round(props.data.temperature)}</span>
-       <span className="unit">°C</span>
+
+    <WeatherTemperature celsius={props.data.temperature}/>
+       
        </div>
        </div>
         </div>
